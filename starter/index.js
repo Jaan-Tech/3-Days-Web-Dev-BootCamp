763634bfb68f7c0477c8f773e206c2a4b434bc8b
clintjeff2 <clintjeff2@gmail.com>
2024-12-07 21:43:18 +0100

    Added the starter files

---
+++ starter/index.js
@@ -0,0 +1,11 @@
+import { menu } from './data.js';
+
+function scrollMenu(direction) {
+	const scrollInterval = setInterval(function () {}, 10);
+}
+
+function cutText(text) {}
+
+function handleOpenModal(menuId) {
+	setTimeout(function () {}, 600);
+}
